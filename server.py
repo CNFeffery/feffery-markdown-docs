ba0d122ec2425a23f13ff65f80c344f2b7bc1ff4
CNFeffery <fefferypzy@gmail.com>
2023-05-22 15:40:09 +0800

    perf: 优化cdn备用资源切换逻辑

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import re
 import dash
 from flask import request
 
@@ -5,14 +6,44 @@ from flask import request
 class CustomDash(dash.Dash):
 
     def interpolate_index(self, **kwargs):
-        # scripts = kwargs.pop('scripts').replace('https://unpkg.com/', 'https://unpkg.zhimg.com/')
-        # scripts = kwargs.pop('scripts').replace('https://unpkg.com/', 'http://npm.elemecdn.com/')
-        # scripts = kwargs.pop('scripts').replace('https://unpkg.com/', 'https://fastly.jsdelivr.net/npm/')
-
+        # scripts = (
+        #     kwargs.pop('scripts').replace(
+        #         'https://unpkg.com/', 'https://npm.elemecdn.com/'
+        #     )
+        # )
         scripts = kwargs.pop('scripts')
 
-        return super(CustomDash, self).interpolate_index(scripts=scripts, **kwargs)
+        # 提取scripts部分符合条件的外部js资源
+        external_scripts = re.findall(
+            '(<script src="http.*?"></script>)', scripts)
 
+        # 将原有的script标签内容替换为带备用地址错误切换的版本
+        for external_script in external_scripts:
+            scripts = scripts.replace(
+                external_script,
+                '''<script src="{}" onerror='this.remove(); let fallbackScript = document.createElement("script"); fallbackScript.src = "{}"; document.querySelector("footer").appendChild(fallbackScript);'></script>'''.format(
+                    re.findall('"(.*?)"', external_script)[0],
+                    re.findall('"(.*?)"', external_script)[0]
+                    .replace('https://unpkg.com/', 'https://npm.elemecdn.com/')
+                )
+            )
+
+        scripts = '''<script>
+window.onerror = async function(message, source, lineno, colno, error) { 
+    if ( message.includes("DashRenderer") !== -1 ) {
+        while ( typeof renderer === 'undefined' ) {
+            try {
+                var renderer = new DashRenderer();
+            } catch {
+                await new Promise(resolve => setTimeout(resolve, 200));
+            }
+        }
+    }
+}
+</script>
+''' + scripts
+
+        return super(CustomDash, self).interpolate_index(scripts=scripts, **kwargs)
 
 app = CustomDash(
     __name__,
@@ -28,6 +59,7 @@ app = CustomDash(
 app.title = 'feffery-markdown-components在线文档'
 
 server = app.server
+
 
 @app.server.route('/safe-redirect')
 def safe_redirect():
